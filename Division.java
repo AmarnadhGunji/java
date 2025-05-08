c272580262c9ed2bea9283c110be702dc3681a4d
AmarnadhGunji <amarnadhgunji@gmail.com>
2025-05-08 22:26:52 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,13 @@
+import java .util.*;
+public class Division {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the value");
+        int a=sc.nextInt(); 
+        int b=sc.nextInt();
+        int c=a/b;
+        System.out.println(c);
+    }
+    
+}
